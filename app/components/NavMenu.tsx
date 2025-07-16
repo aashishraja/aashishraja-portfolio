a14804e3131d82aee08466f11d933d4a6f4cec34
Aashish Raja <aashishraja34@gmail.com>
2025-07-16 23:51:56 +0100

    Test Deployment3

--- app/components/NavMenu.tsx
+++ app/components/NavMenu.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState} from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
 const NavMenu = () => {
--- app/components/ProjectsHero.tsx
+++ app/components/ProjectsHero.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Autoplay from "embla-carousel-autoplay"
 import React from 'react'
-import { Card, CardContent } from "@/components/ui/card"
+
 import ProjectCard from './ProjectCard'
 import {
   Carousel,
